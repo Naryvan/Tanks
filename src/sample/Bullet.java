37dbd15a89e175b6b13062eefab458e532d5f13b
M-Bell <maxymshyjko@gmail.com>
2021-06-03 23:46:39 +0300

    Fix

---
+++ src/sample/Bullet.java
@@ -0,0 +1,39 @@
+package sample;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Bullet {
+
+    double bulletX;
+    double bulletY;
+    double angle;
+
+    GraphicsContext gc;
+
+    public Bullet(double x, double y, double angle, GraphicsContext gc){
+        this.bulletX = x;
+        this.bulletY = y;
+        this.angle = angle;
+        this.gc = gc;
+    }
+
+
+
+    public void renderBullet(){
+        gc.save();
+        gc.setFill(Color.BLACK);
+        gc.strokeOval((bulletX-5) + (Math.cos(Math.toRadians(angle)))*30, (bulletY-5) + (Math.sin(Math.toRadians(angle)))*30, 10, 10);
+        gc.fillOval((bulletX-5) + (Math.cos(Math.toRadians(angle)))*30, (bulletY-5) + (Math.sin(Math.toRadians(angle)))*30, 10, 10);
+    }
+
+    public void moveBullet(){
+        bulletX += (Math.cos(Math.toRadians(angle)))*10;
+        bulletY += (Math.sin(Math.toRadians(angle)))*10;
+    }
+
+}
