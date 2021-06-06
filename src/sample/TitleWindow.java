5d575717cb12bf4f18f96d4ee8e9b84ed11ec3a9
M-Bell <maxymshyjko@gmail.com>
2021-06-06 15:08:00 +0300

    Sound fixed

--- src/sample/TitleWindow.java
+++ src/sample/TitleWindow.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
@@ -19,12 +18,18 @@ public class TitleWindow extends Application {
         launch(args);
     }
 
-    AudioClip mainTheme = new AudioClip(getClass().getResource("/music/main_theme.mp3").toExternalForm());
+    MediaPlayer mediaPlayer;
+    private void music(){
+        String name = "src/music/main_theme.mp3";
+        Media media = new Media(Paths.get(name).toUri().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+    }
 
     @Override
     public void start(Stage primaryStage) {
-        mainTheme.setCycleCount(AudioClip.INDEFINITE);
-        mainTheme.play();
+        music();
         LevelMenu[] levels = StartMenuController.getLevels();
         for (int i = 0; i < levels.length; i++) {
             levels[i] = new LevelMenu("#level" + i, "#backLevelPane" + i);
